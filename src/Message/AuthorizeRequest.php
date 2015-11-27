bf6b0a87b8019f4a5091fef60ff468cb6658ac22
Aaron Guise <aaron.guise@ultrafast.co.nz>
2015-11-27 17:29:13 +1300

    Refactored parameter routines.

--- src/Message/AuthorizeRequest.php
+++ src/Message/AuthorizeRequest.php
@@ -27,33 +27,4 @@ class AuthorizeRequest extends AbstractRequest
         return $this->response = new Response($this, $data);
     }
     
-    public function setAccountId($value)
-    {
-         return $this->setParameter('AccountId', $value);
-    }
-    
-    public function getAccountId()
-    {
-         return $this->getParameter('AccountId');
-    }
-    
-    public function setUsername($value)
-    {
-         return $this->setParameter('Username', $value);
-    }
-    
-    public function getUsername()
-    {
-         return $this->getParameter('Username');
-    }
-    
-    public function setPassword($value)
-    {
-         return $this->setParameter('Password', $value);
-    }
-    
-    public function getPassword()
-    {
-         return $this->getParameter('Password');
-    }
 }
