375488a520899f267fef767979b38178c33f2619
Aaron Guise <aaron.guise@ultrafast.co.nz>
2015-11-30 11:19:19 +1300

    Add DeleteCardRequest

--- src/Message/DeleteCardRequest.php
+++ src/Message/DeleteCardRequest.php
@@ -7,9 +7,26 @@
  */
 
 namespace Omnipay\Flo2cash\Message;
-
+use DOMDocument;
+use SimpleXMLElement;
 
 class DeleteCardRequest extends AbstractRequest
 {
+    public function getData()
+    {
+        $this->validate('cardReference');
+        $TransactionType = 'RemoveCard';
 
+
+        $data = new SimpleXMLElement("<$TransactionType></$TransactionType>",
+            LIBXML_NOERROR,
+            false,
+            '',
+            true);
+        $data->addAttribute('xmlns', 'http://www.flo2cash.co.nz/webservices/paymentwebservice');
+        $data->Username = $this->getUsername();
+        $data->Password = $this->getPassword();
+        $data->CardToken = $this->getCardReference();
+        return array('Transaction' => $TransactionType, 'Data' => $data);
+    }
 }
