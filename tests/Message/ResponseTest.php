8b7bbfc6f5540b406ef0ed68482eb0a536b61c8d
Aaron Guise <aaron.guise@ultrafast.co.nz>
2015-12-02 09:29:18 +1300

    Removed unused tests

--- tests/Message/ResponseTest.php
+++ tests/Message/ResponseTest.php
@@ -81,29 +81,4 @@ class ResponseTest extends TestCase
         $this->assertTrue($response->isSuccessful());
         $this->assertEquals('Refund Successful', $response->getMessage());
     }
-    //public function testSuccess()
-    //{
-    //    $response = new Response(
-    //        $this->getMockRequest(),
-    //        array('reference' => 'abc123', 'success' => 1, 'message' => 'Success')
-    //    );
-    //
-    //    $this->assertTrue($response->isSuccessful());
-    //    $this->assertFalse($response->isRedirect());
-    //    $this->assertSame('abc123', $response->getTransactionReference());
-    //    $this->assertSame('Success', $response->getMessage());
-    //}
-    //
-    //public function testFailure()
-    //{
-    //    $response = new Response(
-    //        $this->getMockRequest(),
-    //        array('reference' => 'abc123', 'success' => 0, 'message' => 'Failure')
-    //    );
-    //
-    //    $this->assertFalse($response->isSuccessful());
-    //    $this->assertFalse($response->isRedirect());
-    //    $this->assertSame('abc123', $response->getTransactionReference());
-    //    $this->assertSame('Failure', $response->getMessage());
-    //}
 }
