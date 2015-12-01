e5e3b8d668a0d4e2e7c554145ec19a25f21a30d8
Aaron Guise <aaron.guise@ultrafast.co.nz>
2015-12-01 16:55:20 +1300

    Added listener to catch 500 errors thrown by gateway

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -51,18 +51,30 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
             "SOAPAction" => $this->getNamespace() . '/' . $TransactionType ,
             "Content-length" => strlen($xml));
 
+        # Catch naughty 500 errors thrown by the gateway
+        $this->httpClient->getEventDispatcher()->addListener(
+            'request.error',
+            function ($event) {
+                if ($event['response']->isServerError()) {
+                    $event->stopPropagation();
+                }
+            }
+        );
+
         $httpRequest = $this->httpClient->post(
             $this->getEndpoint(),
             $headers,
             $xml
         );
-        $req = (string) $httpRequest;
+
+        $request = (string) $httpRequest;
         $file = 'log.txt';
         // Write the contents to the file,
         $data = array('The Request:'. "\n", $req . "\n\n");
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
         file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+        
         $httpResponse = $httpRequest->send();
         $request = (string) $httpResponse;
         $file = 'log.txt';
